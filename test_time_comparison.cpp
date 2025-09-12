5faf4e1e5b7d226059becc94f6233fc8762dd4fb
AlexDonahueKSU <adonahu9@kent.edu>
2025-09-12 13:38:10 -0400

    Final changes

--- test_time_comparison.cpp
+++ test_time_comparison.cpp
@@ -4,9 +4,12 @@
 int main() {
     std::cout << "Begin unit test for: bool operator==(const Time& rhs)\n";
     {
+        std::cout << "Test 2 default constructors\n";
+        Time t1();
+        Time t2();
+        // assert(t1.time() == t2.time());
         
     }
-     
     std::cout << "Done testing bool operator==(const Time& rhs)\n";
     return 0;
 }
